54fd512ea153884c7aa7ed47c29e55205acf9c93
Aleks Zi <aleksandar.zlateski@gmail.com>
2020-07-13 23:12:57 -0400

    updating submodule to latest

--- arm_test.cpp
+++ arm_test.cpp
@@ -45,9 +45,9 @@ public:
     {
         // using namespace Xbyak;
 
-        eor(x4, x4, x4);
+        // eor(x4, x4, x4);
 
-        ld4((v0.b - v3.b), ptr(x0));
+        // ld4((v0.b - v3.b), ptr(x0));
 
         // ld4({v0.s4 - v3.s4}, ptr(x0));
         // ldp(q24, q25, post_ptr(x1, 64));
@@ -58,28 +58,31 @@ public:
 
         // stp(q8, q9, ptr(x2));
 
-        ldp(q0, q1, ptr(x0));
-        facebook::sysml::aot::multi_vreg<VReg> mv(2, 0);
-        //mv.full_reduce(*this, 3);
+        // ldp(q0, q1, ptr(x0));
+        // facebook::sysml::aot::multi_vreg<VReg> mv(2, 0);
+        // //mv.full_reduce(*this, 3);
 
-        // mov(v0.s[0], w4);
-        // dup(v0.s4, v0.s[0]);
-        //fmov(v0.s4, 0.0);
+        // // mov(v0.s[0], w4);
+        // // dup(v0.s4, v0.s[0]);
+        // //fmov(v0.s4, 0.0);
 
-        st1(v0.s4, ptr(x1));
+        // st1(v0.s4, ptr(x1));
 
-        // st1(v1.s4, ptr(x1));
+        // // st1(v1.s4, ptr(x1));
 
 
-        sub(sp, sp, 256);
-        mov(w2, 16);
-        str(w2, ptr(sp, 32));
-        ldr(w0, ptr(sp, 32));
-        add(sp, sp, 256);
+        // sub(sp, sp, 256);
+        // mov(w2, 16);
+        // str(w2, ptr(sp, 32));
+        // ldr(w0, ptr(sp, 32));
+        // add(sp, sp, 256);
 
-        eor(x0, x0, x0);
-        //eors(x0, x0, x0);
-        cmp(x0, 0);
+        // eor(x0, x0, x0);
+        // //eors(x0, x0, x0);
+        // cmp(x0, 0);
+
+        ld1(v1.s4, ptr(x0));
+        st1(v1.s4, ptr(x1));
 
         ret();
     }
