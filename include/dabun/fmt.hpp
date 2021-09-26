40291259a9bf7b5f68f0e218ad5d03e822628a25
Aleks Zi <zi@no.mach>
2021-09-26 07:28:56 -0400

    added libfmt

---
+++ include/dabun/fmt.hpp
@@ -0,0 +1,18 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#define FMT_HEADER_ONLY
+
+#include "fmt/chrono.h"
+#include "fmt/format.h"
+
+#include "fmt/chrono.h"  // date and time formatting
+#include "fmt/color.h"   // terminal color and text style
+#include "fmt/compile.h" // format string compilation
+#include "fmt/os.h"      // system APIs
+#include "fmt/ostream.h" // std::ostream support
+#include "fmt/printf.h"  // printf formatting
+#include "fmt/ranges.h"  // formatting of ranges and tuples
+
+#undef FMT_HEADER_ONLY
