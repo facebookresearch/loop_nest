2885d90ea59fb2f27603c951483bce5e53bdc163
Aleks Zi <zi@no.mach>
2021-10-07 00:45:36 -0400

    loop_nest_stress test working for fp16

--- apps/loop_nest_stress_test.cpp
+++ apps/loop_nest_stress_test.cpp
@@ -1,8 +1,11 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
 #include "baselines.hpp"
 #include "loop_nest_baseline.hpp"
 
 #include "dabun/arithmetic_operation.hpp"
 #include "dabun/check.hpp"
+#include "dabun/isa.hpp"
 #include "dabun/loop_nest.hpp"
 #include "dabun/measure.hpp"
 #include "dabun/random_vector.hpp"
@@ -23,9 +26,23 @@
 #define DABUN_ISA avx2
 #endif
 
+#ifndef DABUN_ARITHMETIC
+#define DABUN_ARITHMETIC float
+#endif
+
+#ifndef DABUN_VEX
+#if defined(DABUN_ARCH_AARCH64)
+#define DABUN_VEX ::dabun::extension::neon
+#else
+#define DABUN_VEX ::dabun::extension::avx2
+#endif
+#endif
+
 int main()
 {
     using namespace dabun;
+
+    using float_t = DABUN_ARITHMETIC;
 
     for (int rounds = 0; rounds < 1000000; ++rounds)
     {
@@ -62,7 +79,10 @@ int main()
                         if (o.second != 1)
                         {
                             o.second = round_up(
-                                o.second, isa_traits<DABUN_ISA>::vector_size);
+                                o.second,
+                                isa_traits<extension_to_deprecated_ISA_t<
+                                        DABUN_VEX>>::vector_size *
+                                    4 / sizeof(float_t));
                         }
                     }
                     std::cout << o.first << "=" << o.second << "  ";
@@ -75,7 +95,7 @@ int main()
                 std::cout << "MU=" << max_fmas_unrolled << std::endl;
 
                 auto fn =
-                    loop_nest_code_generator<DABUN_ISA>(
+                    loop_nest_compiler<DABUN_VEX, float_t>(
                         full_order, // The second argument is a map of the
                                     // dimension sizes
                         {{"AcBr", AcBr}, {"ArCr", ArCr}, {"BcCc", BcCc}},
@@ -94,10 +114,10 @@ int main()
                         max_fmas_unrolled, nullptr)
                         .get_shared();
 
-                auto A = get_random_vector<float>(AcBr * ArCr);
-                auto B = get_random_vector<float>(AcBr * BcCc);
+                auto A = get_random_vector<float_t>(AcBr * ArCr);
+                auto B = get_random_vector<float_t>(AcBr * BcCc);
 
-                auto CN = get_random_vector<float>(ArCr * BcCc);
+                auto CN = get_random_vector<float_t>(ArCr * BcCc);
                 auto CJ = CN;
 
                 baseline_MM(ArCr, AcBr, BcCc, AcBr, 1, BcCc, 1, BcCc, 1,
